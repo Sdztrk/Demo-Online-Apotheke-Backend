f4cace73d00bf22ffb80735a2ce1a0aca71bf23f
sdztrk <msaidozturk1@gmail.com>
2024-03-02 00:46:36 +0100

    index editted again for the build

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors({
  }))
 
  // set static folder 
- app.use(express.static(path.join(__dirname, 'build')));
+ app.use(express.static(path.join(__dirname, "frontend", 'build')));
  
  app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
